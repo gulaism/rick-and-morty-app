f4d51865fc51ed8810130e8cd23078399d640d03
Gulay Movlamova (IT) <gmovlamova@azercell.com>
2026-04-08 14:58:19 +0400

    added: search design

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header";
 import { Characters } from "./characters/characters";
+import { Search } from "./search/search";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Characters],
+  imports: [RouterOutlet, Header, Characters, Search],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
